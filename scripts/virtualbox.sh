b516950776b468df2a75359d08395ad7b78d2b52
Tyler Yahn <tyler@moonshadowmobile.com>
2015-08-13 11:33:03 -0700

    Update virtualbox.sh to properly install guest additions.

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# Bail if we are not running inside VirtualBox.
-if [[ `facter virtual` != "virtualbox" ]]; then
+# Check we are running inside VirtualBox.
+if [[ $( dmidecode | egrep -q 'Product Name: VirtualBox' ) -ne 0 ]]; then
     exit 0
 fi
 
-mkdir -p /mnt/virtualbox
-mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
-sh /mnt/virtualbox/VBoxLinuxAdditions.run
-ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
-umount /mnt/virtualbox
-rm -rf /home/vagrant/VBoxGuest*.iso
+echo "Installing needed packages for Virtualbox Guest Additions"
+apt-get -qq -y install linux-headers-$(uname -r) build-essential dkms
+
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+VBOX_ISO=/home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso
+
+if [ ! -f $VBOX_ISO ] ; then
+    echo "No Virtualbox Guest Additions ISO found"
+    ls -lh /home/vagrant
+    exit 1
+fi
+
+echo "Installing Virtualbox Guest Additions with X11 support"
+mount -o loop $VBOX_ISO /mnt
+sh /mnt/VBoxLinuxAdditions.run --nox11
+umount /mnt
+
+echo "Removing Virtualbox Guest Additions build packages"
+rm $VBOX_ISO
+apt-get -qq -y remove linux-headers-$(uname -r) build-essential
+apt-get -qq -y autoremove
