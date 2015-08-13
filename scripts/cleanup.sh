453e2ba4787677421654e4e0c9f6deac229eb430
Tyler Yahn <tyler@moonshadowmobile.com>
2015-08-13 11:30:36 -0700

    Update cleanup.sh script.
    Remove generic package purge.  This is moved to the relevant script.
    
    Capatalize logging messages.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-# Clean up packages
-echo "cleaning up unneeded packages and package cache"
-apt-get -y remove linux-headers-$(uname -r) build-essential
-apt-get -y autoremove
-apt-get -y purge $(dpkg --list |grep '^rc' |awk '{print $2}')
-apt-get -y purge $(dpkg --list |egrep 'linux-image-[0-9]' |awk '{print $3,$2}' |sort -nr |tail -n +2 |grep -v $(uname -r) |awk '{ print $2}')
-apt-get -y clean
+echo "Cleaning up unneeded packages and package cache"
+apt-get -qq -y purge $(dpkg --list |grep '^rc' |awk '{print $2}')
+apt-get -qq -y purge $(dpkg --list |egrep 'linux-image-[0-9]' |awk '{print $3,$2}' |sort -nr |tail -n +2 |grep -v $(uname -r) |awk '{ print $2}')
+apt-get -qq -y autoremove
+apt-get -qq -y clean
 
-# Cleanup Virtualbox
-echo "cleaning up VBoxGuestAdditions ISOs"
+echo "Cleaning up VBoxGuestAdditions ISOs"
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 
-# Removing leftover leases and persistent rules
-echo "cleaning up dhcp leases"
+echo "Cleaning up dhcp leases"
 rm /var/lib/dhcp/*
 
-# Make sure Udev doesn't block our network
-echo "cleaning up udev rules"
+echo "Cleaning up udev rules"
 rm /etc/udev/rules.d/70-persistent-net.rules
 mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
@@ -26,10 +21,10 @@ rm /lib/udev/rules.d/75-persistent-net-generator.rules
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
 
-echo "cleaning up bash history"
+echo "Cleaning up bash history"
 unset HISTFILE
 [ -f /root/.bash_history ] && rm /root/.bash_history
 [ -f /home/vagrant/.bash_history ] && rm /home/vagrant/.bash_history
 
-echo "cleaning up log files"
+echo "Cleaning up log files"
 find /var/log -type f | while read f; do echo -ne '' > $f; done
